6fc1107272025adfd89909c5f1cabe1e94b9b326
mikhailmaximov <a4UmNlwVNGp>
2021-04-29 20:32:39 +0600

    5

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,23 +1,59 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const { stat} = require(`fs`);
+const {rm} = require('fs/promises');
+const { pipeline } = require('stream');
+const fs = require(`fs`);
+const LimitSizeStream = require('./LimitSizeStream');
+const LimitExceededError = require('./LimitExceededError');
 
 const server = new http.Server();
+server.on('request', async (req, res) => {
+    const pathname = url.parse(req.url).pathname.slice(1);
+    const filepath = path.join(__dirname, 'files', pathname);
 
-server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+    req.on(`close`, ()=>{
+      if (req.aborted) {
+        console.error('REQ aborted');
+        fs.unlinkSync(filepath);
+      }
+    });
+    if(pathname.includes(`/`))
+    {
+      res.statusCode = 400;
+      return res.end('статус код ответа 400');
+    }
+    if(fs.existsSync(filepath))
+    {
+      res.statusCode = 409;
+      return  res.end('возвращается ошибка 409 при создании файла, который есть');
+    }
+    switch (req.method) {
+      case 'POST':
+        let writeFileStream = new fs.createWriteStream(filepath,{flags: 'wx'});
+        let limitSizeStream = new  LimitSizeStream({limit: 1048576});
+        req.pipe(limitSizeStream).pipe(writeFileStream);
+        limitSizeStream.on(`error`,(error)=>{ console.error('Pipeline failed.');
+          if(error instanceof(LimitExceededError)) {
+            fs.unlinkSync(filepath);
+            res.statusCode = 413;
+            return res.end('file is too big');
+          }} );
 
-  const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'POST':
+            writeFileStream.on(`close`,()=>{console.log('Pipeline succeeded.');
+              res.statusCode = 201;
+              return  res.end('статус код ответа сервера 200');});
 
-      break;
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+        break;
+
+
+      default:
+        res.statusCode = 501;
+        res.end('Not implemented');
+    }
 });
 
 module.exports = server;
